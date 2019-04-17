a9f1206ca4d706487735af6e0b5af5cbff50c74b
Kim HyunWoo <stories2@naver.com>
2019-04-17 14:26:53 +0900

    리엑트 라이프 사이클

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -23,7 +23,24 @@ const movies = [
 ]
 
 class App extends Component {
+
+  //Render: componentWillMount -> render -> componentDidMount
+
+  //Update: componentWillReceiveProps -> shouldComponentUpdate -> componentWillUpdate -> render -> componentDidUpdate
+
+  componentWillMount() {
+    // Load Data
+    console.log("componentWillMount")
+  }
+
+  componentDidMount() {
+    // After Edit Data
+    console.log("componentDidMount")
+  }
+
   render() {
+    // Render Data
+    console.log("render")
     return (
       <div className="App">
         {movies.map((movie, index) => {
