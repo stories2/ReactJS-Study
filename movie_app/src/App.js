7f4e7562d1198ff56858d6e6db96d6f04db2d6b6
Kim HyunWoo <stories2@naver.com>
2019-04-17 15:27:17 +0900

    데이터의 상태가 바뀌면 레이아웃 다시 렌더링

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -3,25 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie'
 
-const movies = [
-  {
-    title: "Matrix",
-    poster: "https://upload.wikimedia.org/wikipedia/tr/thumb/3/36/Matrix-film.jpg/220px-Matrix-film.jpg"
-  },
-  {
-    title: "Full Metal Jacket",
-    poster: "https://i.pinimg.com/736x/36/1e/cd/361ecdb85a3767f70810cbe2cdaaf1a4.jpg"
-  },
-  {
-    title: "Oldboy",
-    poster: "https://images-na.ssl-images-amazon.com/images/I/5177K3F5Y0L._SY445_.jpg"
-  },
-  {
-    title: "Star Wars",
-    poster: "https://images-na.ssl-images-amazon.com/images/I/81WjGytz7HL._SY445_.jpg"
-  }
-]
-
 class App extends Component {
 
   //Render: componentWillMount -> render -> componentDidMount
@@ -36,6 +17,41 @@ class App extends Component {
   componentDidMount() {
     // After Edit Data
     console.log("componentDidMount")
+
+    setTimeout(() => {
+      // this.state.greeting = "Bye"
+      this.setState({
+        movies: [
+          ...this.state.movies,
+          {
+            title: "트랜스포머 2007",
+            poster: "https://upload.wikimedia.org/wikipedia/ko/thumb/e/e4/%ED%8A%B8%EB%9E%9C%EC%8A%A4%ED%8F%AC%EB%A8%B8_%EC%98%81%ED%99%94.jpg/250px-%ED%8A%B8%EB%9E%9C%EC%8A%A4%ED%8F%AC%EB%A8%B8_%EC%98%81%ED%99%94.jpg"
+          }
+        ]
+      })
+    }, 5000)
+  }
+
+  state = {
+    greeting: "Hello",
+    movies: [
+      {
+        title: "Matrix",
+        poster: "https://upload.wikimedia.org/wikipedia/tr/thumb/3/36/Matrix-film.jpg/220px-Matrix-film.jpg"
+      },
+      {
+        title: "Full Metal Jacket",
+        poster: "https://i.pinimg.com/736x/36/1e/cd/361ecdb85a3767f70810cbe2cdaaf1a4.jpg"
+      },
+      {
+        title: "Oldboy",
+        poster: "https://images-na.ssl-images-amazon.com/images/I/5177K3F5Y0L._SY445_.jpg"
+      },
+      {
+        title: "Star Wars",
+        poster: "https://images-na.ssl-images-amazon.com/images/I/81WjGytz7HL._SY445_.jpg"
+      }
+    ]
   }
 
   render() {
@@ -43,7 +59,7 @@ class App extends Component {
     console.log("render")
     return (
       <div className="App">
-        {movies.map((movie, index) => {
+        {this.state.movies.map((movie, index) => {
           return (
             <Movie title={movie.title} poster={movie.poster} key={index}/>
           )
