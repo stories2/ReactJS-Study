d8f2ac4b8d8443ef63cb901fe5e0efeaf4d3ba00
Kim HyunWoo <stories2@naver.com>
2019-04-17 15:47:32 +0900

    상태에 따라서 영화를 보여줄지 로딩을 나타낼지 결정함

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -22,7 +22,22 @@ class App extends Component {
       // this.state.greeting = "Bye"
       this.setState({
         movies: [
-          ...this.state.movies,
+          {
+            title: "Matrix",
+            poster: "https://upload.wikimedia.org/wikipedia/tr/thumb/3/36/Matrix-film.jpg/220px-Matrix-film.jpg"
+          },
+          {
+            title: "Full Metal Jacket",
+            poster: "https://i.pinimg.com/736x/36/1e/cd/361ecdb85a3767f70810cbe2cdaaf1a4.jpg"
+          },
+          {
+            title: "Oldboy",
+            poster: "https://images-na.ssl-images-amazon.com/images/I/5177K3F5Y0L._SY445_.jpg"
+          },
+          {
+            title: "Star Wars",
+            poster: "https://images-na.ssl-images-amazon.com/images/I/81WjGytz7HL._SY445_.jpg"
+          },
           {
             title: "트랜스포머 2007",
             poster: "https://upload.wikimedia.org/wikipedia/ko/thumb/e/e4/%ED%8A%B8%EB%9E%9C%EC%8A%A4%ED%8F%AC%EB%A8%B8_%EC%98%81%ED%99%94.jpg/250px-%ED%8A%B8%EB%9E%9C%EC%8A%A4%ED%8F%AC%EB%A8%B8_%EC%98%81%ED%99%94.jpg"
@@ -33,25 +48,17 @@ class App extends Component {
   }
 
   state = {
-    greeting: "Hello",
-    movies: [
-      {
-        title: "Matrix",
-        poster: "https://upload.wikimedia.org/wikipedia/tr/thumb/3/36/Matrix-film.jpg/220px-Matrix-film.jpg"
-      },
-      {
-        title: "Full Metal Jacket",
-        poster: "https://i.pinimg.com/736x/36/1e/cd/361ecdb85a3767f70810cbe2cdaaf1a4.jpg"
-      },
-      {
-        title: "Oldboy",
-        poster: "https://images-na.ssl-images-amazon.com/images/I/5177K3F5Y0L._SY445_.jpg"
-      },
-      {
-        title: "Star Wars",
-        poster: "https://images-na.ssl-images-amazon.com/images/I/81WjGytz7HL._SY445_.jpg"
-      }
-    ]
+    greeting: "Hello"
+  }
+
+  _renderMovieView = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return (
+        <Movie title={movie.title} poster={movie.poster} key={index}/>
+      )
+    })
+
+    return movies
   }
 
   render() {
@@ -59,11 +66,7 @@ class App extends Component {
     console.log("render")
     return (
       <div className="App">
-        {this.state.movies.map((movie, index) => {
-          return (
-            <Movie title={movie.title} poster={movie.poster} key={index}/>
-          )
-        })}
+        {this.state.movies ? this._renderMovieView() : 'Loading'}
       </div>
     );
   }
