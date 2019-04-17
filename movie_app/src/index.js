aac1840e1910ea0dcb86b6e3d7a54a8ef14c705d
Kim HyunWoo <stories2@naver.com>
2019-04-17 10:48:56 +0900

    App.js 건들여보기

--- movie_app/src/index.js
+++ movie_app/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
+// App 컴포넌트를 index.html 의 root 라는 ID 를 가진 영역에 작성을 해라
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
