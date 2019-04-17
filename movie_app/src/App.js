c906969114d1162d7a7446dbe4ee6973b7f51300
Kim HyunWoo <stories2@naver.com>
2019-04-17 13:46:06 +0900

    리엑트는 여러 데이터를 보여줄때 unique key가 필요하다

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -26,9 +26,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {movies.map(movie => {
+        {movies.map((movie, index) => {
           return (
-            <Movie title={movie.title} poster={movie.poster}/>
+            <Movie title={movie.title} poster={movie.poster} key={index}/>
           )
         })}
       </div>
