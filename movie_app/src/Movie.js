6ba659ebf80c5308790f48d7dcf7e7b4d5a33bee
Kim HyunWoo <stories2@naver.com>
2019-04-18 16:02:00 +0900

    상태가 없는 컴포넌트로 리팩토링

--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -1,8 +1,8 @@
-import React, { Component} from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import './Movie.css'
 
-class Movie extends Component{
+class Movie extends Component {
 
     static propTypes = {
         title: PropTypes.string.isRequired,
@@ -11,17 +11,29 @@ class Movie extends Component{
 
     render() {
         console.log(this.props)
+        // return (
+        //     <div>
+        //         <h1>Movie Title: {this.props.title}</h1>
+        //         {/* <MoviePoster poster={this.props.poster}></MoviePoster> */}
+        //         <MoviePosterStateless poster={this.props.poster}/>
+        //     </div>
+        // )
         return (
-            <div>
-                <h1>Movie Title: {this.props.title}</h1>
-                {/* <MoviePoster poster={this.props.poster}></MoviePoster> */}
-                <MoviePosterStateless poster={this.props.poster}/>
-            </div>
+            <MovieStateless title={this.props.title} poster={this.props.poster} />
         )
     }
 }
 
-class MoviePoster extends Component{
+function MovieStateless({ title, poster }) {
+    return (
+        <div>
+            <h1>Movie Title: {title}</h1>
+            <MoviePosterStateless poster={poster} />
+        </div>
+    )
+}
+
+class MoviePoster extends Component {
 
     static propTypes = {
         poster: PropTypes.string.isRequired
@@ -34,10 +46,14 @@ class MoviePoster extends Component{
     }
 }
 
-function MoviePosterStateless({poster}) {
+function MoviePosterStateless({ poster }) {
     return (
-        <img src={poster} alt="Movie Img"/>
+        <img src={poster} alt="Movie Img" />
     )
+}
+
+MoviePosterStateless.propTypes = {
+    poster: PropTypes.string.isRequired
 }
 
 export default Movie
